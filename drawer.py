091588570f67586acef0d9de6bcb2bad19fa3cd6
miki4920 <miki4920@gmail.com>
2022-07-26 12:22:25 +0100

    Added Turtle to draw walls

--- drawer.py
+++ drawer.py
@@ -11,3 +11,14 @@ with open("test.dungeondraft_map", "r") as file:
     height = world["height"] * 256
     walls = world["levels"]["0"]["walls"]
 
+listen()
+screensize(canvwidth=width, canvheight=height)
+s = getscreen()
+turtle = Turtle()
+for wall in walls:
+    coordinates = [int(number) for number in re.findall(r"([0-9]+).? ", wall["points"])]
+    turtle.penup()
+    turtle.goto(coordinates[0]-1024, coordinates[1]-1024)
+    turtle.pendown()
+    turtle.forward(100)
+done()
