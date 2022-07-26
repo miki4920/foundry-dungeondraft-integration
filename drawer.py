fe4f1a688883f22028b8fbfd3c986170bb65f1b3
miki4920 <miki4920@gmail.com>
2022-07-26 14:44:23 +0100

    Added a full dungeondraft drawing for walls

--- drawer.py
+++ drawer.py
@@ -2,10 +2,18 @@ import json
 import re
 from turtle import *
 
-def map_coordinates(x, y, x_size, y_size):
-    x = x-(x_size/2)
 
-with open("test.dungeondraft_map", "r") as file:
+def x_coordinates_mapper(x, x_size):
+    x = x-(x_size/2)
+    return x
+
+
+def y_coordinates_mapper(y, y_size):
+    y = -(y-(y_size/2))
+    return y
+
+
+with open("Dungeon_1.dungeondraft_map", "r") as file:
     world = json.load(file)["world"]
     width = world["width"] * 256
     height = world["height"] * 256
@@ -15,10 +23,19 @@ listen()
 screensize(canvwidth=width, canvheight=height)
 s = getscreen()
 turtle = Turtle()
+turtle.speed(0)
 for wall in walls:
     coordinates = [int(number) for number in re.findall(r"([0-9]+).? ", wall["points"])]
+    start_coordinates = coordinates[:2]
+    wall_coordinates = coordinates[2:]
     turtle.penup()
-    turtle.goto(coordinates[0]-1024, coordinates[1]-1024)
+    turtle.goto(x_coordinates_mapper(start_coordinates[0], width), y_coordinates_mapper(start_coordinates[1], height))
     turtle.pendown()
-    turtle.forward(100)
+    while len(coordinates) != 0:
+        turtle.goto(x_coordinates_mapper(coordinates[0], width), y_coordinates_mapper(coordinates[1], height))
+        coordinates = coordinates[2:]
+    if wall["loop"]:
+        turtle.goto(x_coordinates_mapper(start_coordinates[0], width),
+                    y_coordinates_mapper(start_coordinates[1], height))
+
 done()
