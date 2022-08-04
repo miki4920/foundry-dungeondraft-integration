faf18a49da749eb868a7d4c4099d40d8fbb9b17d
miki4920 <miki4920@gmail.com>
2022-08-04 19:21:50 +0100

    Updated colours in dark maps

--- scripts/module.js
+++ scripts/module.js
@@ -1,12 +1,12 @@
 const regex = /(-?[0-9]+\.?[0-9]*)[^(]? /g
 
-function convertVectorIntoArray(vector, regex, scale=true) {
+function convertVectorIntoArray(vector, regex, scale = true) {
     vector = [...vector.matchAll(regex)]
     let array = []
-    for(let number of vector) {
+    for (let number of vector) {
         number = parseFloat(number[1])
         if (scale) {
-            number = (number/256)*canvas.scene.dimensions.size
+            number = (number / 256) * canvas.scene.dimensions.size
         }
         array.push(number)
     }
@@ -15,13 +15,13 @@ function convertVectorIntoArray(vector, regex, scale=true) {
 
 function drawWalls(walls) {
     let numericalWalls = []
-    for(let wallInfo of walls) {
+    for (let wallInfo of walls) {
         let wall = convertVectorIntoArray(wallInfo.points, regex)
-        for(let i=0; i<wall.length-2;i+=2) {
-            numericalWalls.push({c: [wall[i], wall[i+1], wall[i+2], wall[i+3]]})
+        for (let i = 0; i < wall.length - 2; i += 2) {
+            numericalWalls.push({c: [wall[i], wall[i + 1], wall[i + 2], wall[i + 3]]})
         }
-        if(wallInfo.loop) {
-            numericalWalls.push({c: [wall[0], wall[1], wall[wall.length-2], wall[wall.length-1]]})
+        if (wallInfo.loop) {
+            numericalWalls.push({c: [wall[0], wall[1], wall[wall.length - 2], wall[wall.length - 1]]})
         }
     }
     canvas.scene.createEmbeddedDocuments("Wall", numericalWalls)
@@ -35,27 +35,41 @@ function applyRotation(array, angle) {
 
 function drawPortals(portals) {
     let numericalPortals = []
-    for(let portalInfo of portals) {
+    for (let portalInfo of portals) {
         let portal = convertVectorIntoArray(portalInfo.position, regex);
         let scale = convertVectorIntoArray(portalInfo.scale, regex, false)
         let rotation = [canvas.scene.dimensions.size, 0]
         rotation = applyRotation(rotation, portalInfo.rotation)
         rotation = [rotation[0] * scale[0], rotation[1] * scale[1]]
-        numericalPortals.push({c: [portal[0]-rotation[0]/2, portal[1]-rotation[1]/2, portal[0] + rotation[0]/2, portal[1] + rotation[1]/2], door: 1})
+        numericalPortals.push({
+            c: [portal[0] - rotation[0] / 2, portal[1] - rotation[1] / 2, portal[0] + rotation[0] / 2, portal[1] + rotation[1] / 2],
+            door: 1
+        })
     }
     canvas.scene.createEmbeddedDocuments("Wall", numericalPortals)
 }
 
 function drawLights(lights) {
     let numericalLights = []
-    for(let lightInfo of lights) {
+    for (let lightInfo of lights) {
         let light = convertVectorIntoArray(lightInfo.position, regex)
-        let dimLight = lightInfo.range*canvas.scene.dimensions.distance
-        let brightLight = dimLight*(lightInfo.intensity/3)
+        let dimLight = lightInfo.range * canvas.scene.dimensions.distance
+        let brightLight = dimLight * (lightInfo.intensity / 3)
         let colorLight = "#" + lightInfo.color.substring(2, lightInfo.color.length)
-        numericalLights.push({x: light[0], y: light[1],
-            config: {dim: dimLight, bright: brightLight,
-                color: colorLight, contrast: 1}})
+        numericalLights.push({
+            x: light[0], y: light[1],
+            config: {
+                dim: dimLight,
+                bright: brightLight,
+                color: colorLight,
+                alpha: 0.25,
+                contrast: 0,
+                gradual: true,
+                luminosity: 0.5,
+                saturation: 0.25,
+                shadows: 0.15,
+            }
+        })
 
     }
     canvas.scene.createEmbeddedDocuments("AmbientLight", numericalLights)
@@ -63,16 +77,16 @@ function drawLights(lights) {
 
 function drawPaths(paths) {
     let numericalPaths = []
-    for(let pathInfo of paths) {
+    for (let pathInfo of paths) {
         let pathPosition = convertVectorIntoArray(pathInfo.position, regex)
         let initialCoordinates = [...pathPosition]
         let path = convertVectorIntoArray(pathInfo.edit_points, regex)
-        for(let i=0; i<path.length-1;i+=2) {
-            numericalPaths.push({c: [initialCoordinates[0] + path[i], initialCoordinates[1] + path[i+1], pathPosition[0], pathPosition[1]]})
+        for (let i = 0; i < path.length - 1; i += 2) {
+            numericalPaths.push({c: [initialCoordinates[0] + path[i], initialCoordinates[1] + path[i + 1], pathPosition[0], pathPosition[1]]})
             pathPosition[0] = initialCoordinates[0] + path[i]
-            pathPosition[1] = initialCoordinates[1] + path[i+1]
+            pathPosition[1] = initialCoordinates[1] + path[i + 1]
         }
-        if(pathInfo.loop) {
+        if (pathInfo.loop) {
             numericalPaths.push({c: [initialCoordinates[0], initialCoordinates[1], pathPosition[0], pathPosition[1]]})
         }
     }
@@ -106,7 +120,7 @@ async function drawScene() {
 
 Hooks.on("getSceneControlButtons", (controls) => {
     let wallTools = controls.find((x) => x["name"] === "walls").tools;
-    wallTools.splice(wallTools.length-2, 0, {
+    wallTools.splice(wallTools.length - 2, 0, {
         icon: "fas fa-landmark",
         name: "dungeondraftButton",
         title: "Draw Dungeondraft Walls",
